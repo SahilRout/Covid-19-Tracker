ad0046cc59e82724d4908d80bcbc81ab57b37965
Sahil <sahilrout07@gmail.com>
2021-07-10 14:55:55 +0530

    commit on master

--- lib/pages/charts.dart
+++ lib/pages/charts.dart
@@ -9,56 +9,149 @@ class Charts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: primaryBlack,
       appBar: AppBar(
         title: Text('PieChart'),
       ),
       body: worlddata.isEmpty
           ? Center(child: CircularProgressIndicator())
           : Center(
-              child: Container(
-                margin: EdgeInsets.all(10),
-                height: 300,
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color(0xFF0A0E21),
-                      blurRadius: 2.0,
-                      spreadRadius: 0.0,
-                      offset:
-                          Offset(2.0, 2.0), // shadow direction: bottom right
-                    )
-                  ],
-                  color: Colors.blueGrey.shade500,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Center(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'WORLDWIDE 🌏',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22.0,
-                            fontWeight: FontWeight.bold,
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    height: 300,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xFF0A0E21),
+                          blurRadius: 2.0,
+                          spreadRadius: 0.0,
+                          offset: Offset(
+                              2.0, 2.0), // shadow direction: bottom right
+                        )
+                      ],
+                      color: Colors.blueGrey.shade500,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'WORLDWIDE 🌏',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
-                        ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Center(
+                                child: Container(
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'CONFIRMED: ' +
+                                            worlddata['cases'].toString(),
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 28,
+                                            color: Color(0xFFFF7575)),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        'ACTIVE: ' +
+                                            worlddata['active'].toString(),
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 28,
+                                            color: Color(0xFF74B7FE)),
+                                      ),
+                                      SizedBox(
+                                        height: 5,
+                                      ),
+                                      Text(
+                                        'RECOVERED: ' +
+                                            worlddata['recovered'].toString(),
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 28,
+                                            color: Color(0xFF56EFC3)),
+                                      ),
+                                      Text(
+                                        'DEATHS: ' +
+                                            worlddata['deaths'].toString(),
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 28,
+                                            color: Color(0xFFFFE9A8)),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              )),
+                        ],
                       ),
-                      PieChart(
-                        chartType: ChartType.ring,
-                        dataMap: {
-                          'CONFIRMED': worlddata['cases'].toDouble(),
-                          'ACTIVE': worlddata['active'].toDouble(),
-                          'RECOVERED': worlddata['recovered'].toDouble(),
-                          'DEATHS': worlddata['deaths'].toDouble()
-                        },
-                        animationDuration: Duration(milliseconds: 800),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    height: 300,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0xFF0A0E21),
+                          blurRadius: 2.0,
+                          spreadRadius: 0.0,
+                          offset: Offset(
+                              2.0, 2.0), // shadow direction: bottom right
+                        )
+                      ],
+                      color: Colors.blueGrey.shade500,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'WORLDWIDE 🌏',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: PieChart(
+                              chartType: ChartType.ring,
+                              dataMap: {
+                                'CONFIRMED': worlddata['cases'].toDouble(),
+                                'ACTIVE': worlddata['active'].toDouble(),
+                                'RECOVERED': worlddata['recovered'].toDouble(),
+                                'DEATHS': worlddata['deaths'].toDouble()
+                              },
+                              animationDuration: Duration(milliseconds: 800),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
     );
--- lib/pages/stateschart.dart
+++ lib/pages/stateschart.dart
@@ -11,57 +11,143 @@ class StatesCharts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: primaryBlack,
       appBar: AppBar(
         title: Text('PieChart'),
       ),
       body: statedata.isEmpty
           ? Center(child: CircularProgressIndicator())
           : Center(
-              child: Container(
-                margin: EdgeInsets.all(10),
-                height: 300,
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 2.0,
-                      spreadRadius: 0.0,
-                      offset:
-                          Offset(2.0, 2.0), // shadow direction: bottom right
-                    )
-                  ],
-                  color: Colors.blueGrey.shade500,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Center(
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          statedata[index]['loc'],
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 22.0,
-                            fontWeight: FontWeight.bold,
+              child: Column(
+                children: [
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    height: 300,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 2.0,
+                          spreadRadius: 0.0,
+                          offset: Offset(
+                              2.0, 2.0), // shadow direction: bottom right
+                        )
+                      ],
+                      color: Colors.blueGrey.shade500,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              statedata[index]['loc'],
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
-                        ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text(
+                                    'CONFIRMED: ' +
+                                        statedata[index]['totalConfirmed']
+                                            .toString(),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 28,
+                                        color: Color(0xFFFF7575)),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    'RECOVERED: ' +
+                                        statedata[index]['discharged']
+                                            .toString(),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 28,
+                                        color: Color(0xFF56EFC3)),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text(
+                                    'DEATHS: ' +
+                                        statedata[index]['deaths'].toString(),
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 28,
+                                        color: Color(0xFF74B7FE)),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                      PieChart(
-                        chartType: ChartType.ring,
-                        dataMap: {
-                          'CONFIRMED':
-                              statedata[index]['totalConfirmed'].toDouble(),
-                          // 'ACTIVE': statedata[index]['active'].toDouble(),
-                          'RECOVERED':
-                              statedata[index]['discharged'].toDouble(),
-                          'DEATHS': statedata[index]['deaths'].toDouble()
-                        },
-                      ),
-                    ],
+                    ),
                   ),
-                ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    height: 300,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 2.0,
+                          spreadRadius: 0.0,
+                          offset: Offset(
+                              2.0, 2.0), // shadow direction: bottom right
+                        )
+                      ],
+                      color: Colors.blueGrey.shade500,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              statedata[index]['loc'],
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: PieChart(
+                              chartType: ChartType.ring,
+                              dataMap: {
+                                'CONFIRMED': statedata[index]['totalConfirmed']
+                                    .toDouble(),
+                                // 'ACTIVE': statedata[index]['active'].toDouble(),
+                                'RECOVERED':
+                                    statedata[index]['discharged'].toDouble(),
+                                'DEATHS': statedata[index]['deaths'].toDouble()
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
     );
